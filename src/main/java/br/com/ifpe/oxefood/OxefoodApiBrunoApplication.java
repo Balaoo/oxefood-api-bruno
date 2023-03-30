f2fe0f2a5b0ef64b2c865fa724304438e1a9c82b
alunoo <alunoo@ifpe>
2023-03-30 17:20:07 -0300

    aula 5

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiBrunoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiBrunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiBrunoApplication.class, args);
+	}
+
+}
