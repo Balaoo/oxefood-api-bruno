a3375e55c4cf15007d5becae31a79a264cf35e10
alunoo <alunoo@ifpe>
2023-04-03 15:04:22 -0300

    metade do slide 9

--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeAuditavel.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeAuditavel.java
@@ -2,11 +2,14 @@ package br.com.ifpe.oxefood.util.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.MappedSuperclass;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class EntidadeAuditavel extends EntidadeNegocio {
     
     private Long versao;
--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
@@ -2,12 +2,15 @@ package br.com.ifpe.oxefood.util.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.MappedSuperclass;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
 @EqualsAndHashCode(of = { "id" })
 public abstract class EntidadeNegocio implements Serializable {
 
